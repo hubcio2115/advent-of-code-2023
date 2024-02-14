74cb80a3cd1c0cd07972df1a3332c8da719cba69
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-02-14 18:41:45 +0100

    day 1

---
+++ src/days/day_1.rs
@@ -0,0 +1,84 @@
+use std::{collections::HashMap, str::Lines, usize};
+
+use regex::Regex;
+
+pub fn part_1(lines: Lines) -> usize {
+    let pattern = Regex::new(r"\d").unwrap();
+
+    lines
+        .map(|line| -> usize {
+            let mut number_matches = pattern.find_iter(line);
+
+            let left = number_matches.next().unwrap();
+            let right = number_matches.last().unwrap_or(left);
+
+            format!("{}{}", left.as_str(), right.as_str())
+                .parse()
+                .unwrap()
+        })
+        .sum()
+}
+
+pub fn part_2(lines: Lines) -> usize {
+    let digits: HashMap<String, usize> = "one,two,three,four,five,six,seven,eight,nine"
+        .split(',')
+        .map(|s| s.to_string())
+        .zip(1..=9)
+        .chain((0..=9).map(|i| (i.to_string(), i)))
+        .collect();
+
+    let res: usize = lines
+        .map(|line| {
+            let first_digit = digits
+                .iter()
+                .map(|(digit_string, value)| (line.find(digit_string), *value))
+                .filter(|(index, _)| index.is_some())
+                .min()
+                .unwrap()
+                .1;
+            let last_digit = digits
+                .iter()
+                .map(|(digit_string, &value)| (line.rfind(digit_string), value))
+                .max()
+                .unwrap()
+                .1;
+            first_digit * 10 + last_digit
+        })
+        .sum();
+
+    return res;
+}
+
+#[cfg(test)]
+mod test {
+    use crate::days::day_1::{part_1, part_2};
+
+    #[test]
+    fn part_1_works() {
+        let lines = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"
+            .lines();
+
+        let result = part_1(lines);
+
+        assert_eq!(result, 142);
+    }
+
+    #[test]
+    fn part_2_works() {
+        let lines = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"
+            .lines();
+
+        let result = part_2(lines);
+
+        assert_eq!(result, 281);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![allow(clippy::needless_return)]
+
+use std::fs;
+mod days;
+
+fn main() {
+    if let Ok(file) = fs::read_to_string("./input.txt") {
+        let lines = file.lines();
+
+        let result_part1 = days::day_1::part_1(lines.clone());
+
+        println!("{}", result_part1);
+
+        let result_part2 = days::day_1::part_2(lines.clone());
+
+        println!("{}", result_part2);
+    } else {
+        eprintln!("Couldn't read the input file.")
+    }
+}
