cbb85eabf4ec26affa0e64c6b6ec9a0d988b3379
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-02-15 15:08:17 +0100

    day_2

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,11 @@ fn main() {
     if let Ok(file) = fs::read_to_string("./input.txt") {
         let lines = file.lines();
 
-        let result_part1 = days::day_1::part_1(lines.clone());
+        let result_part1 = days::day_2::part_1(lines.clone());
 
         println!("{}", result_part1);
 
-        let result_part2 = days::day_1::part_2(lines.clone());
+        let result_part2 = days::day_2::part_2(lines.clone());
 
         println!("{}", result_part2);
     } else {
